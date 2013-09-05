f9bf5cd89925b9d347b9529790957bce94ec203d
Tyler Campbell <tylerwcampbell@live.com>
2013-09-04 17:21:22 -0700

    first commit, blank application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webcrib::Application.config.secret_key_base = 'ffefd8cc5dd00a411271b6b5621072bf176c71c2c8bc227402023736e8ae5e2e5f9ef095e10afb1464aeca72e050beaab717273b534121957dbf496040a1a6af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Webcrib::Application.config.session_store :cookie_store, key: '_webcrib_session'
